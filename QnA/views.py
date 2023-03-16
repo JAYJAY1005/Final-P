6fcb1da2445a7edb616c61e17634c7645d0ba456
JAY <keol8740@naver.com>
2023-03-16 12:40:42 +0900

    질문,답변 란 비었을 경우 오류 문자 발생 구현

--- QnA/views.py
+++ QnA/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
-
+from .forms import QuestionForm, AnswerForm
+from django.http import HttpResponseNotAllowed
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -14,6 +15,32 @@ def detail(request, question_id):
     return render(request, 'QnA/question_detail.html', context)
 
 def answer_create(request, question_id):
+    """
+    QnA 답변등록
+    """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('QnA:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('QnA:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'QnA/question_detail.html', context)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('QnA:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'QnA/question_form.html', context)
